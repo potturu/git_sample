4392678d605fcac3068c93d32c8c91978724c57f
chihiro <tsurutachihiro@akane.waseda.jp>
2021-07-10 22:15:01 +0900

    基本プログラムの作成

--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,21 @@
 #include<iostream>
+using namespace std;
+ int main(){
+  int A, B;
+  string op;
+  //「3 * 4」のように，（整数）[スペース]（演算子）[スペース]（整数）の形で入力
+  cin >> A >> op >> B;
 
-int sum(int, int);
+  if (op == "+") {
+    cout << A + B << endl;
+  }else if(op == "*") {
+        cout<< A * B << endl;
+  }else if (op=="-"){
+      cout <<A - B <<endl;
+  }else if(op == "/"){
+      cout<<A / B<<endl; 
+  }
 
-int main()
-{
-    int a=1; int b=9;
-    std::cout<<sum(a,b)<<std::endl;
-    return 0;
 
-}
-
-int sum(int a, int b)
-{
-    return a+b;
+  return 0;
 }
